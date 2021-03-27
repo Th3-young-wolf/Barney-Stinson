1e2ff96acea2f35739cd3475dccdde4a4b4042b3
Adithya Krishan <79042374+Th3-young-wolf@users.noreply.github.com>
2021-03-27 17:30:12 +0530

    Update misc.py

--- tg_bot/modules/misc.py
+++ tg_bot/modules/misc.py
@@ -416,7 +416,65 @@ def getsticker(bot: Bot, update: Update):
         bot.sendChatAction(chat_id, "typing")
         update.effective_message.reply_text("Hello " + "[{}](tg://user?id={})".format(msg.from_user.first_name,
                                             msg.from_user.id) + ", Please reply to sticker message to get sticker image",
-                                            parse_mode=ParseMode.MARKDOWN)
+    
+
+
+
+                                        parse_mode=ParseMode.MARKDOWN)
+
+
+
+WALL_API = (
+        "awoo"  # For wallpapers, get one from https://wall.alphacoders.com/api.php
+    )
+@run_async
+def wall(bot: Bot, update: Update):
+    chat_id = update.effective_chat.id
+    msg = update.effective_message
+    args = args = update.effective_message.text.split(None, 1)
+    msg_id = update.effective_message.message_id
+    query = " ".join(args)
+    if not query:
+        msg.reply_text("Please enter a query!")
+        return
+    else:
+        caption = query
+        term = query.replace(" ", "%20")
+        json_rep = r.get(
+            f"https://wall.alphacoders.com/api2.0/get.php?auth={WALL_API}&method=search&term={term}"
+        ).json()
+        if not json_rep.get("success"):
+            msg.reply_text(f"An error occurred! Report this @{SUPPORT_CHAT}")
+        else:
+            wallpapers = json_rep.get("wallpapers")
+            if not wallpapers:
+                msg.reply_text("No results found! Refine your search.")
+                return
+            else:
+                index = randint(0, len(wallpapers) - 1)  # Choose random index
+                wallpaper = wallpapers[index]
+                wallpaper = wallpaper.get("url_image")
+                wallpaper = wallpaper.replace("\\", "")
+                bot.send_photo(
+                    chat_id,
+                    photo=wallpaper,
+                    caption="Preview",
+                    reply_to_message_id=msg_id,
+                    timeout=60,
+                )
+                bot.send_document(
+                    chat_id,
+                    document=wallpaper,
+                    filename="wallpaper",
+                    caption=caption,
+                    reply_to_message_id=msg_id,
+                    timeout=60,
+                )
+
+
+WALLPAPER_HANDLER = DisableAbleCommandHandler("wall", wall)
+dispatcher.add_handler(WALLPAPER_HANDLER)
+
 
 # /ip is for private use
 __help__ = """
